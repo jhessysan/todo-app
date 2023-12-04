7c1c413549d41392c356f8e34c66b7e59bb2ab91
jhessysan <jecilene_santana@educadventista.org>
2023-12-04 20:48:25 -0300

    iniciando leitura dos dados do banco de dados

--- index.js
+++ index.js
@@ -36,6 +36,25 @@ app.post('/criar', (requisicao, resposta) =>{
 })
 
 app.get('/', (requisicao, resposta)=> {
+    const sql = 'SELECT * FROM tarefas'
+
+    conexao.query(sql, (erro, dados) =>{
+        if(erro){
+            return console.log(ero)
+        }
+
+        const tarefas = dados.map((dado) =>{
+            return{
+                id: dado.id,
+                descricao: dado.descricao,
+                completa: dado.completa === 0? false : true
+            }
+        })
+
+        
+    })
+
+
     resposta.render('home')
 })
 
